d72d895b3b03ef30c292bd59f7f3033ad1b8c3de
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2025-02-17 19:49:28 +0100

    feat(auth):Implemented Clerk for authenticated -Add sign page -Add sign out page

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,12 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import React from 'react'
 
 const Document= () => {
@@ -9,6 +16,13 @@ const Document= () => {
         <div className='flex w-fit gap-2 justify-between'>
           <p className='document-title'>head</p>
         </div>
+          <SignedOut>
+                <SignInButton />
+                {/* <SignUpButton /> */}
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
       </Header>
       <Editor/>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 export const metadata:Metadata = {
@@ -13,10 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
